d3cb1d97d8f77a7a86ef0e66fdeb14e2ca2cb25e
Tyler Nex <tylernex@gmail.com>
2025-05-06 13:40:19 -0700

    print statements now optional

--- cribbagecounter.py
+++ cribbagecounter.py
@@ -5,7 +5,11 @@ import random
 
 class CribbageCounter:
     def __init__(self):
-        pass
+        self.reporting = False
+        
+    def report(self, string):
+        if self.reporting:
+            print(string)
     
     def count_runs(self, hand: list[Card]) -> int:
         cards = [0] * 13
@@ -26,14 +30,14 @@ class CribbageCounter:
                         subtotal *= multiplier
                     points += subtotal
                 first = last
-        print(f"{points}pts from runs: {cards}")
+        self.report(f"{points}pts from runs: {cards}")
         return points
     
     def count_fifteens(self, hand: list[Card]) -> int:
         card_values = [card.value for card in hand]
         fifteens = [x for x in more_itertools.powerset(card_values) if sum(x) == 15]
         points = 2 * len(fifteens)
-        print(f"{points}pts from fifteens: {fifteens}")
+        self.report(f"{points}pts from fifteens: {fifteens}")
         return points
     
     def count_flushes(self, hand: list[Card]) -> int:
@@ -41,12 +45,12 @@ class CribbageCounter:
             return len(hand)
         for card in hand[2:]:
             if card.suit != hand[1].suit:
-                print("no flush")
+                self.report("no flush")
                 return 0
         flush_size = len(hand)
         if hand[0].suit == hand[1].suit:
             flush_size += 1
-        print(f"flush of {flush_size}")
+        self.report(f"flush of {flush_size}")
         return flush_size
     
     def count_pairs(self, hand: list[Card]) -> int:
@@ -55,26 +59,26 @@ class CribbageCounter:
             cards[card.number - 1] += 1
         cards = [2*comb(card, 2) for card in cards]
         points = sum(cards)
-        print(f"{points}pts from pairs: {cards}")
+        self.report(f"{points}pts from pairs: {cards}")
         return points
     
     def count_hand(self, hand: list[Card]) -> int:
         hand.sort(key=lambda card : card.number)
-        print(', '.join(map(str, hand)))
+        self.report(', '.join(map(str, hand)))
         total = self.count_runs(hand=hand)
         total += self.count_fifteens(hand=hand)
         total += self.count_flushes(hand=hand)
         total += self.count_pairs(hand=hand)
         if len(hand) > 1 and len([card for card in hand[1:] if card.number == 11 and card.suit == hand[0].suit]) > 0:
             total += 1
-            print("and one for the jack")
+            self.report("and one for the jack")
         
         return total
 
 
 if __name__ == "__main__":
-    num_hands = 100
-    hand_range = 10
+    num_hands = 10000
+    hand_range = 15
     random.seed()
     deck = Deck()
     average_hands = [0] * hand_range
